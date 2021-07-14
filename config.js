91157a4d98510cab90d4eb080cd50b6a56c369b8
iemgaicuaanh <87405126+iemgaicuaanh@users.noreply.github.com>
2021-07-14 11:05:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    introTitle: 'Hí anh traii!',
+    introDesc: `Sinh nhật vui vẻ sinh đẻ thành công`,
+    btnIntro: 'okii iemm',
+    title: 'Iem biết nay sinh nhật anh, nhưng mà, em sẽ ko mua bánh cho anh đou😏',
+    desc: 'tại sao hả ? Tại anh phải mua bánh cho iem chứ 😂😂 ',
+    btnYes: 'Oke iemmm <33',
+    btnNo: 'Không , m bị ảo à:3',
+    question:'Mua cho iem 10 cái bánh đc ko',
+    btnReply: 'Gửi cho iemm <3',
+    reply: '100 cái cx đựt iem owii <33333333',
+    mess: 'Đùa thôi,chúc mừng sênh nhật anh trai !!!!',
+    messDesc: 'tối nhớ phần iem bánh đấy!!',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
